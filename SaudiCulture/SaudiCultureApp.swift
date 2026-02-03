523f3922b2323a379df2a04619dd78ed1c3f5d6b
Haya Almousa <hayaf2211@gmail.com>
2026-02-03 14:01:02 +0300

    Initial Commit

---
+++ SaudiCulture/SaudiCultureApp.swift
@@ -0,0 +1,17 @@
+//
+//  SaudiCultureApp.swift
+//  SaudiCulture
+//
+//  Created by Haya almousa on 03/02/2026.
+//
+
+import SwiftUI
+
+@main
+struct SaudiCultureApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
