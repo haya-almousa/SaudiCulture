ef57502bd94d254c8f8e9f322d0c81a23c35ab20
Haya Almousa <hayaf2211@gmail.com>
2026-02-03 17:45:52 +0300

    d

--- SaudiCulture/name.swift
+++ SaudiCulture/name.swift
@@ -8,16 +8,29 @@
 import SwiftUI
 
 struct Name: View {
+    @State private var name: String = ""
+
     var body: some View {
-        ZStack{
+        ZStack {
             Color("BackgroundMain")
                 .ignoresSafeArea()
-            Text("أكتب اسمك هنا")
-                .font(.custom("Saudi-Regular", size: 28))
-                .foregroundStyle(Color("brown"))
+
+            VStack(spacing: 5) {
+                TextField("أكتب اسمك هنا", text: $name)
+                    .font(.custom("Saudi-Regular", size: 30))
+                    .foregroundStyle(Color("black"))
+                    .multilineTextAlignment(.center)
+
+                Rectangle()
+                    .frame(height: 3)
+                    .foregroundStyle(Color("brown"))
+                    .padding(.horizontal, 60)
+            }
+            .padding()
         }
     }
 }
+
 #Preview {
     Name()
 }
