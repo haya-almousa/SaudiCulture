6756f6ae617f8f33e23d9b3b94d1ebbe8e58d031
Rawan <rawanalgarny21@gmail.com>
2026-02-09 17:02:31 +0300

    ..

--- SaudiCulture/CardGameRegionsTests/CardGameRegionsTests.swift
+++ SaudiCulture/CardGameRegionsTests/CardGameRegionsTests.swift
@@ -5,15 +5,15 @@
 //  
 //
 
-//import Testing
-//@testable import CardGameRegions
-//
-//struct CardGameRegionsTests {
-//
-//    @Test func example() async throws {
-//        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
-//    }
-//
-//}
+import Testing
+@testable import CardGameRegions
+
+struct CardGameRegionsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
 //
 
