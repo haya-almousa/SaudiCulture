b9e882865a47298e6f039f5fbe03f6ce452b5a1d
wed <wed>
2026-02-10 17:58:07 +0300

    level

--- SaudiCulture/LEVEL.swift
+++ SaudiCulture/LEVEL.swift
@@ -5,3 +5,76 @@
 //  Created by Wed Ahmed Alasiri on 22/08/1447 AH.
 //
 
+
+import SwiftUI
+
+struct StackedCirclesView: View {
+    
+    let totalLevels = 5
+    @State private var unlockedLevel = 0
+    
+    var body: some View {
+        ZStack {
+            
+            Image("background")
+                .resizable()
+                .scaledToFill()
+                .ignoresSafeArea()
+            
+            ZStack {
+
+                // المراحل
+                ForEach(0..<totalLevels, id: \.self) { index in
+
+                    Image(index <= unlockedLevel ? "yellowCircle" : "grayCircle")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: index == 0 ? 110 : 90)
+                        .offset(y: yPosition(for: index))
+
+                        // نخلي الضغط فقط على المرحلة المفتوحة الحالية
+                        .onTapGesture {
+                            if index == unlockedLevel {
+                                nextLevel()
+                            }
+                        }
+                }
+
+                // الشخصية
+                Image("netImage")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 70)
+                    .offset(y: yPosition(for: unlockedLevel))
+                    .animation(
+                        .spring(response: 0.6, dampingFraction: 0.7),
+                        value: unlockedLevel
+                    )
+            }
+            .offset(y: 60)
+
+        }
+        .onTapGesture {
+            nextLevel()
+        }
+    }
+    
+    // تحريك المراحل عموديًا
+    func yPosition(for index: Int) -> CGFloat {
+        let spacing: CGFloat = 110
+        let startFromBottom: CGFloat = 200 // تحكم بمكان البداية من تحت
+        return startFromBottom - CGFloat(index) * spacing
+    }
+
+    
+    // فتح مرحلة جديدة
+    func nextLevel() {
+        if unlockedLevel < totalLevels - 1 {
+            unlockedLevel += 1
+        }
+    }
+
+}
+#Preview {
+    StackedCirclesView()
+}
